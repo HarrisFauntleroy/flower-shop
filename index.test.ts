8cffb4496da0e49584b45149f5597d2b57cb4b2d
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2022-09-26 16:45:13 +1000

    update test

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,6 @@
 import { exampleInput, expectedOutput } from './exampleValues'
 import checkout from './checkout'
 
-
 test("Flower shop checkout", () => {
 	expect(checkout(exampleInput)).toEqual(expectedOutput)
-})
+});
